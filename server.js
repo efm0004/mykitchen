c6ccfb4a25c41ad83fa7c0758c3445921ef74527
Emily Moore <efm0004@gmail.com>
2019-10-11 10:11:45 -0500

    Heroku test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 //Put API routes here
