2ded461cff11b726f81fd36a2a79e2e97bbb5318
Emily Moore <efm0004@gmail.com>
2019-10-10 20:38:49 -0500

    presentation ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, Link, NavLink, Switch, Redirect } from 'react-router-dom';
+import {Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import LoginPage from '../src/pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
