23b49bf6c888130f029e0d898791522db9552784
Emily Moore <efm0004@gmail.com>
2019-10-29 15:54:37 -0500

    stubbed up ShoppingBagPage

---
+++ src/pages/ShoppingBagPage/ShoppingBagPage.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'; 
+
+class ShoppingBagPage extends Component {
+    state = {
+        location: 'ShoppingBag'
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>I am the ShoppingBagPage!</h1>
+            </div> 
+        )
+    }
+
+}
+
+export default ShoppingBagPage;
