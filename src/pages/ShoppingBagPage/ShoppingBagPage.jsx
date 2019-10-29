4fef76e2219c4e605575e487449b242386e358dc
Emily Moore <efm0004@gmail.com>
2019-10-29 15:59:54 -0500

    next steps notes

--- src/pages/ShoppingBagPage/ShoppingBagPage.jsx
+++ src/pages/ShoppingBagPage/ShoppingBagPage.jsx
@@ -16,4 +16,15 @@ class ShoppingBagPage extends Component {
 
 }
 
+//At this point, you should be working while checking in on 
+//what is being rendered in the browser
+
+//still TO DO: 
+//set up backend for ShoppingList
+//set up ShoppingListForm
+//set up ShoppingListEditForm
+//update this file with function - see example from Inventory
+//      in App.js
+//styling!
+
 export default ShoppingBagPage;
